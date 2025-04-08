b6add819ae22631140993e74acbc12c6ac4c8cd0
joannac112 <joannac112@nycstudents.net>
2025-04-08 13:06:34 +0000

    save1

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -8,8 +8,8 @@ public class ArrayResizer {
     public static boolean isNonZeroRow(int[][] array2D, int r) {
         for (int i : array2D[r]){
             if (i == 0) return false;
-            return true;
         }
+        return true;
     }
 
 
@@ -36,6 +36,16 @@ public class ArrayResizer {
      * Postcondition: array2D is unchanged.
      */
     public static int[][] resize(int[][] array2D) {
-        int rows = 
+        int rows = numNonZeroRows(array2D);
+        int cols = array2D[0].length;
+        int[][]arr = new int[rows][cols];
+        int r = 0;
+        for (int row = 0; row < array2D.length; row++){
+            if (isNonZeroRow(array2D, row)){
+                arr[r] = array2D[row];
+                r++;
+            }
+        }
+        return arr;
     }
 }
